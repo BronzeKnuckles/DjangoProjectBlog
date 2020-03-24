e71a4989d48246a7b507a4827aed1848ed1ea531
BronzeKnuckles <srivenkatesh1999@gmail.com>
2020-03-24 16:08:52 +0530

    Initial Commit

---
+++ blog/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+
+
+posts =[
+    {
+        'author': 'venkypro',
+        'title': 'blog post 1 ',
+        'content':'FIRST POST CONTENT',
+        'date_posted': 'August 27, 2018'
+    },
+    {
+        'author': 'venkypro1',
+        'title': 'blog post 12 ',
+        'content':'FIRST POST CONTENT1',
+        'date_posted': 'August 27, 20181'
+    }
+]
+
+
+def home(request):
+    context = {
+        'posts': posts
+    }
+    return render(request,'blog/home.html', context)
+
+def about(request):
+    return render(request,'blog/about.html')
